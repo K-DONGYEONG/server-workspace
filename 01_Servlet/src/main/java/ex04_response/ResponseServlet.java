aff025044932102d341e6111ac90e65eea622677
KIMDONGYEONG <kdy1336@naver.com>
2023-05-09 14:01:06 +0900

    0509

--- 01_Servlet/src/main/java/ex04_response/ResponseServlet.java
+++ 01_Servlet/src/main/java/ex04_response/ResponseServlet.java
@@ -1,6 +1,8 @@
 package ex04_response;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -31,11 +33,40 @@ public class ResponseServlet extends HttpServlet {
 		 * 
 		 * 	1. 서버 -> 클라이언트 보내는 것이 응답(response)이다.
 		 * 	2. HttpServletResponse클래스가 응답을 처리함.
-		 * 	3. 어떤 MIME타입으로 응답할 것인지 결정해야함.
+		 * 	3. 어떤 MIME타입으로 응답할 것인지 결정해야함. (MIME타입은 클라이언트에게 전송된 문서의 다양성을 알려주기 위한 메커니즘입니다.)
 		 * 			1) HTML : text/html 	=> 태그를 만들어서 반환하는 경우
 		 * 			2) XML : application/xml	=> ajax 응답이 XML인 경우
 		 * 			3) JSON : application/json => ajax 응답이 JSON인 경우
 		 */
+		// 파라미터에서 값 추출하기
+		String model = request.getParameter("model");
+		String strPrice = request.getParameter("price");
+		
+		int price = 0;
+		if(strPrice !=null) {
+			price = Integer.parseInt(strPrice);
+		}
+		// 응답 만들기
+		
+		// 1. 응답할 데이터의 MIME 타입과 인코딩 설정
+		response.setContentType("text/html; charset=utf-8");
+		
+		// 2. 응답 스크림 생성(IOException 처리가 필요함. 서블릿 생성시 이미 처리되어 있음.)
+		PrintWriter out = response.getWriter(); //PrintWriter의 출력 메서드 : append(), write(), print(), println()
+		
+		// 3. 응답 만들기
+		out.println("<html>");
+		out.println("<head>");
+		out.println("<title>서블릿 응답</title>");
+		out.println("</head>");
+		out.println("<body>");
+		out.println("<h1> 모델명 : " + model + "</h1><br>");
+		out.println("<h1> 가격 : " + price + "</h1><br>");
+		out.println("</body>");
+		out.println("</html>");
+		
+		out.flush();
+		out.close();
 	}
 
 	/**
