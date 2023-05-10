4ca67141d1d969aee2fcf535f92467e5e649da48
KIMDONGYEONG <kdy1336@naver.com>
2023-05-10 15:34:25 +0900

    0510

---
+++ 03_Member/src/main/java/com/goodee/model/dao/memberDao.java
@@ -0,0 +1,15 @@
+package com.goodee.model.dao;
+
+import java.util.ArrayList;
+
+import com.goodee.model.vo.memberVO;
+
+public class memberDao {
+	// 1) JDBC 이용해서 DB연결
+	// 2) Connection, PreparedStatement, ResultSet 이용해서 전체 멤버 조회하기
+	// 3) 조회된 멤버를 memberVO 객체를 ArrayList<memberVO>이용해서 담기 담기
+	// 4) ArrayList<memberVO>객체 반환하기
+	public ArrayList<memberVO> selectMember() {
+		ArrayList<memberVO> list = new ArrayList<>;
+	}
+}
